33804325697dfb92b8158deb6b1efe156c8baf63
Dev Sajnani <sajnanidev@berkeley.edu>
2024-03-06 17:13:34 -0800

    organizing files

---
+++ EZero/core/test_save.py
@@ -0,0 +1,233 @@
+import os
+import ray
+import time
+import torch
+
+import numpy as np
+import core.ctree.cytree as cytree
+
+from tqdm.auto import tqdm
+from torch.cuda.amp import autocast as autocast
+from core.mcts import MCTS
+from core.game import GameHistory
+from core.utils import select_action, prepare_observation_lst
+
+
+@ray.remote(num_gpus=0.25)
+def _test_save(config, shared_storage):
+    test_model = config.get_uniform_network()
+    best_test_score = float('-inf')
+    episodes = 0
+    while True:
+        counter = ray.get(shared_storage.get_counter.remote())
+        if counter >= config.training_steps + config.last_steps:
+            time.sleep(30)
+            break
+        if counter >= config.test_interval * episodes:
+            episodes += 1
+            test_model.set_weights(ray.get(shared_storage.get_weights.remote()))
+            test_model.eval()
+
+            test_score, eval_steps, _ = test_save(config, test_model, counter, config.test_episodes, config.device, False, save_video=False)
+            mean_score = test_score.mean()
+            std_score = test_score.std()
+            print('Start evaluation at step {}.'.format(counter))
+            if mean_score >= best_test_score:
+                best_test_score = mean_score
+                torch.save(test_model.state_dict(), config.model_path)
+
+            test_log = {
+                'mean_score': mean_score,
+                'std_score': std_score,
+                'max_score': test_score.max(),
+                'min_score': test_score.min(),
+            }
+
+            shared_storage.add_test_log.remote(counter, test_log)
+            print('Training step {}, test scores: \n{} of {} eval steps.'.format(counter, test_score, eval_steps))
+
+        time.sleep(30)
+
+class SaveFeatures():
+    activations=None
+    def __init__(self, targ_lay):
+        if targ_lay.to_hook == 'input':
+            self.hook = targ_lay.layer.register_forward_hook(self.hook_in)
+        else:
+            self.hook = targ_lay.layer.register_forward_hook(self.hook_out)
+            
+        self.activations = targ_lay.activations
+    def hook_out(self, module, input, output): self.activations.append(output.detach().cpu())
+    def hook_in(self, module, input, output): self.activations.append(input[0].detach().cpu())
+    def remove(self): self.hook.remove()
+
+#attaches a target layer of the model and file folder to the saved activations
+class target_layer():
+    def __init__(self, layer, folder, to_hook='output'):
+        self.layer = layer
+        self.to_hook = to_hook
+        self.folder = folder
+        #store activations as a list of pytorch tensors, then when it's big, save to disk and increment the chunk number
+        self.activations = []
+        self.chunk_number = 0
+
+
+def save_chunk(targ_lay):
+    #save stored activations to disk at folder/chunk number.pt
+    dataset = torch.cat(targ_lay.activations, dim=0).to('cpu')
+    os.makedirs(targ_lay.folder, exist_ok=True)
+    with open(targ_lay.folder+"/"+str(targ_lay.chunk_number)+".pt","wb") as f:
+        torch.save(dataset, f)
+    #wipe stored activations
+    targ_lay.activations = []
+    targ_lay.chunk_number += 1
+
+
+def test_save(config, model, counter, test_episodes, device, render, save_video=False, final_test=False, use_pb=False):
+    """evaluation test
+    Parameters
+    ----------
+    model: any
+        models for evaluation
+    counter: int
+        current training step counter
+    test_episodes: int
+        number of test episodes
+    device: str
+        'cuda' or 'cpu'
+    render: bool
+        True -> render the image during evaluation
+    save_video: bool
+        True -> save the videos during evaluation
+    final_test: bool
+        True -> this test is the final test, and the max moves would be 108k/skip
+    use_pb: bool
+        True -> use tqdm bars
+    """
+    model.to(device)
+    model.eval()
+    save_path = os.path.join(config.exp_path, 'recordings', 'step_{}'.format(counter))
+
+    with torch.no_grad():
+        # new games
+        envs = [config.new_game(seed=i, save_video=save_video, save_path=save_path, test=True, final_test=final_test,
+                              video_callable=lambda episode_id: True, uid=i) for i in range(test_episodes)]
+
+
+        max_episode_steps = envs[0].get_max_episode_steps()
+        if use_pb:
+            pb = tqdm(np.arange(max_episode_steps), leave=True)
+        # initializations
+        init_obses = [env.reset() for env in envs]
+        dones = np.array([False for _ in range(test_episodes)])
+        game_histories = [GameHistory(envs[_].env.action_space, max_length=max_episode_steps, config=config) for _ in range(test_episodes)]
+        for i in range(test_episodes):
+            game_histories[i].init([init_obses[i] for _ in range(config.stacked_observations)])
+
+        step = 0
+        ep_ori_rewards = np.zeros(test_episodes)
+        ep_clip_rewards = np.zeros(test_episodes)
+
+        #Initialize target_layers for all the layers we want to hook.
+        hooked_layers = set()
+        hooked_layers.add(target_layer(model.projection[6],"proj/6"))
+        hooked_layers.add(target_layer(model.projection[3],"proj/3"))
+        hooked_layers.add(target_layer(model.prediction_network.conv1x1_policy,"pol/0", to_hook='input'))
+        hooked_layers.add(target_layer(model.prediction_network.bn_policy,"pol/2"))
+        hooked_layers.add(target_layer(model.prediction_network.fc_policy,"pol/4", to_hook='input'))
+        hooked_layers.add(target_layer(model.prediction_network.bn_value,"val/2"))
+        hooked_layers.add(target_layer(model.prediction_network.fc_value,"val/4", to_hook='input'))
+
+        # loop
+        while not dones.all():
+            if render:
+                for i in range(test_episodes):
+                    envs[i].render()
+
+            if config.image_based:
+                stack_obs = []
+                for game_history in game_histories:
+                    stack_obs.append(game_history.step_obs())
+                stack_obs = prepare_observation_lst(stack_obs)
+                stack_obs = torch.from_numpy(stack_obs).to(device).float() / 255.0
+            else:
+                stack_obs = [game_history.step_obs() for game_history in game_histories]
+                stack_obs = torch.from_numpy(np.array(stack_obs)).to(device)
+            
+            #Set hooks
+            temp_hooks = set()
+            for l in hooked_layers:
+                temp_hooks.add(SaveFeatures(l))
+
+            #Call initial inference
+            with autocast():
+                network_output = model.initial_inference(stack_obs.float())
+
+            #Manually call projection network
+            proj = model.project(torch.from_numpy(network_output.hidden_state).to(device), with_grad=False)
+
+            #Save chunks to disk if larger than desired chunk size (100 MB)
+            for l in hooked_layers:
+                if sum(map(lambda x : torch.numel(x)*4, l.activations)) > 10**8:
+                    save_chunk(l)
+
+            #Remove hooks
+            for h in temp_hooks:
+                h.remove()
+
+            hidden_state_roots = network_output.hidden_state
+            reward_hidden_roots = network_output.reward_hidden
+            value_prefix_pool = network_output.value_prefix
+            policy_logits_pool = network_output.policy_logits.tolist()
+
+            roots = cytree.Roots(test_episodes, config.action_space_size, config.num_simulations)
+            roots.prepare_no_noise(value_prefix_pool, policy_logits_pool)
+            # do MCTS for a policy (argmax in testing)
+            MCTS(config).search(roots, model, hidden_state_roots, reward_hidden_roots)
+
+            roots_distributions = roots.get_distributions()
+            roots_values = roots.get_values()
+            for i in range(test_episodes):
+                if dones[i]:
+                    continue
+
+                distributions, value, env = roots_distributions[i], roots_values[i], envs[i]
+                # select the argmax, not sampling
+                action, _ = select_action(distributions, temperature=1, deterministic=True)
+
+                obs, ori_reward, done, info = env.step(action)
+                if config.clip_reward:
+                    clip_reward = np.sign(ori_reward)
+                else:
+                    clip_reward = ori_reward
+
+                game_histories[i].store_search_stats(distributions, value)
+                game_histories[i].append(action, obs, clip_reward)
+
+                dones[i] = done
+                ep_ori_rewards[i] += ori_reward
+                ep_clip_rewards[i] += clip_reward
+
+            step += 1
+
+            if use_pb:
+                total_saved = 0
+                for l in hooked_layers:
+                    total_saved += sum(map(lambda x : torch.numel(x)*4, l.activations))
+                    
+                pb.set_description('{}, trial {}, mean score: {}, stored activations: {}, currently'
+                                   ''.format(config.env_name, counter,
+                                             ep_ori_rewards.mean(), total_saved))
+                pb.update(1)
+
+        for env in envs:
+            env.close()
+
+        #save the tail end of the data
+        for l in hooked_layers:
+            if sum(map(lambda x : torch.numel(x)*4, l.activations)) > 100:
+                save_chunk(l)
+
+
+    return ep_ori_rewards, step, save_path
+
---
+++ EZero/test_neurons.py
@@ -0,0 +1,142 @@
+import torch
+
+import argparse
+import os
+import random
+
+from config.atari import game_config
+from config.atari.heatmap_model import EfficientZeroNetH
+from core.test_write_trace import test_write_trace
+from core.game import GameHistory
+
+
+
+def one_hot(n_neurons, n, device):
+    temp = torch.zeros(n_neurons).to(device)
+    temp[n]+=1
+    return temp
+
+
+#test input: python test_neurons.py --env MsPacmanNoFrameskip-v4 --model_path ./model/model.p --sae_paths ./sae/test_sae.p --random_features=5
+
+if __name__ == '__main__':
+    #gather arguments related to testing:
+    parser = argparse.ArgumentParser(description='EfficientZero')
+    parser.add_argument('--env', required=True, help='Name of the gym environment')
+    parser.add_argument('--test_episodes', type=int, default=1, help='Evaluation episode count (default: %(default)s)')
+    parser.add_argument('--model_path', type=str, default='./results/model.p', help='load model path')
+    parser.add_argument('--sae_paths', nargs='+', type=str, default=['./results/sae.p'], help='load some autoencoder paths')
+    parser.add_argument('--sae_layers', nargs='+', type=str, default=['p6'], help='layers for the loaded autoencoders. Currently recommended: p3, p6, q0, q3, q4, v3, v4')
+    parser.add_argument('--results_path', type=str, default='./results/test_neurons', help='save clips directory')
+    parser.add_argument('--device', type=str, default='cpu', help='cpu or cuda')
+    parser.add_argument('--save_neurons', action='store_true', help='Flag: Attribute neurons if no autoencoder at file')
+    parser.add_argument('--random_features', type=int, default=0, help='Number of random features to attribute (default: %(default)s)')
+    parser.add_argument('--features', nargs='+', type=int, default=[], help='Specific features to attribute if possible')
+    parser.add_argument('--feature_source', type=str, default='encoder', help='pick encoder or decoder - only different if weights are untied')
+
+    args = parser.parse_args()
+    assert os.path.exists(args.model_path), 'model not found at {}'.format(args.model_path)
+    for p in args.sae_paths:
+        if args.save_neurons:
+            if not os.path.exists(p):
+                print('model not found at {}, saving neurons'.format(p))
+        else:
+            assert os.path.exists(p), 'autoencoder not found at {}'.format(p)
+    device = args.device
+    print(args)
+
+    #set configs
+    #implied_args = type('', (object,),{"env":"BreakoutNoFrameskip-v4", "case":"atari","opr":"test","amp_type":"torch_amp","render":False,"seed":0,"use_priority":False,"use_max_priority":False,"debug":False,"device":'cpu',"cpu_actor":4,"gpu_actor":4,"p_mcts_num":4,"use_root_value":False,"use_augmentation":False,"revisit_policy_search_rate":0.99,"result_dir":"./","info":"none"})()
+    implied_args = type('', (object,),{"env":args.env,
+                                        "case":"atari",
+                                        "opr":"test",
+                                        "amp_type":"torch_amp",
+                                        "render":False,
+                                        "seed":0,
+                                        "use_priority":False,
+                                        "use_max_priority":False,
+                                        "debug":False,
+                                        "device":device,
+                                        "cpu_actor":4,
+                                        "gpu_actor":4,
+                                        "p_mcts_num":4,
+                                        "use_root_value":False,
+                                        "use_augmentation":False,
+                                        "revisit_policy_search_rate":0.99,
+                                        "result_dir":args.results_path,
+                                        "info":"none"
+                                        })()
+    exp_path = game_config.set_config(implied_args)
+
+    #initialize model with a custom model that has internal variables and a heatmap method
+    model = EfficientZeroNetH(
+            game_config.obs_shape,
+            game_config.action_space_size,
+            game_config.blocks,
+            game_config.channels,
+            game_config.reduced_channels_reward,
+            game_config.reduced_channels_value,
+            game_config.reduced_channels_policy,
+            game_config.resnet_fc_reward_layers,
+            game_config.resnet_fc_value_layers,
+            game_config.resnet_fc_policy_layers,
+            game_config.reward_support.size,
+            game_config.value_support.size,
+            game_config.downsample,
+            game_config.inverse_value_transform,
+            game_config.inverse_reward_transform,
+            game_config.lstm_hidden_size,
+            bn_mt=game_config.bn_mt,
+            proj_hid=game_config.proj_hid,
+            proj_out=game_config.proj_out,
+            pred_hid=game_config.pred_hid,
+            pred_out=game_config.pred_out,
+            init_zero=game_config.init_zero,
+            state_norm=game_config.state_norm).to(device)
+    
+    #load model
+    model.load_state_dict(torch.load(args.model_path, map_location=torch.device(device)), strict=False)
+
+    #assemble set of feature numbers to test
+    feature_nums = set()
+    #TODO: pick a max number based on actual size of layers rather than a hardcoded constant?
+    n_features = 1024
+    feature_nums.update(random.sample(range(0,n_features),args.random_features))
+    feature_nums.update(args.features)
+
+    #encoders is the set of names of autoencoders - the layer (e.g. p6), followed by the file name
+    autoencoders = set()
+    #Load autoencoder features and make a dict
+    ae_features = {}
+    for n in range(len(args.sae_paths)):
+        ae_name = args.sae_layers[n] + ''.join(filter(str.isalnum,args.sae_paths[n]))
+        autoencoders.add(ae_name)
+
+        #store an autoencoder's offset bias and features in a dict
+        ae_features[ae_name]={}
+        if os.path.exists(args.sae_paths[n]):
+            #load state_dict
+            sd = torch.load(p, map_location=torch.device(device))
+
+            ae_features[ae_name]['offset'] = sd['_post_decoder_bias._bias_reference']
+            #get the desired features
+            #TODO: test compatibility iwth convolutional layers
+            for m in feature_nums:
+                if m < sd['_encoder._weight'].size()[0]:
+                    if args.feature_source == 'decoder':
+                        ae_features[ae_name][m] = (sd['_decoder._weight'][:,m], 0)
+                    elif args.feature_source == 'encoder':
+                        ae_features[ae_name][m] = (sd['_encoder._weight'][m,:], sd['_encoder._bias'][m])
+                    else:
+                        raise ValueError('Pick encoder or decoder')
+
+        else:
+            #construct one-hot features at the index given by the feature numbers
+            #TODO: check compatibility with convolutional layers
+            ae_features[ae_name]['offset'] = torch.zeros(model.features(args.sae_layers[n])).to(device)
+            for m in feature_nums:
+                ae_features[ae_name][m] = (one_hot(model.features(args.sae_layers[n]), m, device),0)
+
+
+    #call test_write_trace to do the thing
+    test_write_trace(game_config, model, args.test_episodes, device, autoencoders, ae_features, feature_nums, use_pb=True)
